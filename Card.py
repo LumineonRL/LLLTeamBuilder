9ee1f8c8085e564c612c0e3082bdc45d6f7ec353
LumineonRL <thebaconator25@aim.com>
2023-05-28 23:50:38 -0400

    Changed some classes to inner classes.

--- Card.py
+++ Card.py
@@ -1,27 +1,54 @@
-class Stats:
-    def __init__(self, smile: int, pure: int, cool: int, mental: int, bp: int) -> None:
-        self.smile = smile
-        self.pure = pure
-        self.cool = cool
-        self.mental = mental
-        self.bp = bp
-
-
-class Mood:
-    def __init__(self, type: str, mood: str) -> None:
-        self.type = type
-        self.mood = mood
-
-
 class Card:
     def __init__(
-        self, name: str, character: str, rarity: str, stats: Stats, mood: Mood
+        self,
+        name: str,
+        character: str,
+        rarity: str,
+        smile: int,
+        pure: int,
+        cool: int,
+        mental: int,
+        bp: int,
+        type: str,
+        mood: str,
     ) -> None:
         self.name = name
         self.character = character
         self.rarity = rarity
-        self.stats = stats
-        self.mood = mood
+        self.stats = self.Stats(smile, pure, cool, mental, bp)
+        self.mood = self.Mood(type, mood)
+        self.training = self.Training()
+
+    class Stats:
+        def __init__(
+            self, smile: int, pure: int, cool: int, mental: int, bp: int
+        ) -> None:
+            self.smile = smile
+            self.pure = pure
+            self.cool = cool
+            self.mental = mental
+            self.bp = bp
+
+    class Mood:
+        def __init__(self, type: str, mood: str) -> None:
+            self.type = type
+            self.mood = mood
+            
+    class Training:
+        def __init__(
+            self,
+            level: int = 1,
+            uncaps: int = 1,
+            appeal_level: int = 1,
+            skill_level: int = 1,
+        ):
+            self.level = level
+            self.uncaps = uncaps
+            self.appeal_level = appeal_level
+            self.skill_level = skill_level
+        
+
+            
 
 
 class MyCards:
@@ -31,7 +58,7 @@ class MyCards:
     def add_card(
         self,
         name: str,
-        charcater: str,
+        character: str,
         rarity: str,
         s: int,
         p: int,
@@ -41,9 +68,7 @@ class MyCards:
         mood_type: str,
         mood_mood: str,
     ) -> None:
-        card_stats = Stats(s, p, c, m, b)
-        card_mood = Mood(mood_type, mood_mood)
-        card = Card(name, charcater, rarity, card_stats, card_mood)
+        card = Card(name, character, rarity, s, p, c, m, b, mood_type, mood_mood)
         self.cards.append(card)
 
     def get_card_by_index(self, index) -> Card:
--- Main.py
+++ Main.py
@@ -2,10 +2,15 @@ from Card import MyCards
 
 if __name__ == "__main__":
     my_cards_collection = MyCards()
-    my_cards_collection.add_card(
-        "オーロラスカイ", "日野下花帆", "R", 1225, 1525, 925, 123, 100, "パフォーマー", "ハッピー"
-    )
+
+    try:
+        my_cards_collection.add_card(
+            "オーロラスカイ", "日野下花帆", "R", 1225, 1525, 925, 123, 100, "パフォーマー", "ハッピー"
+        )
+    except Exception as e:
+        print(e)
     # Get all the names of cards in our collection and print them out.
     # print(my_cards_collection.get_card_names())
-    print(my_cards_collection.get_card_by_index(0).stats.mental)
-    
+    print(my_cards_collection.get_card_by_index(0).training.level)
+    my_cards_collection.get_card_by_index(0).training.level = 100
+    print(my_cards_collection.get_card_by_index(0).training.level)
