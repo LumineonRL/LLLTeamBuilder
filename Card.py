a817269e5ac62ba40e6354a349e44ae3cf7ef677
LumineonRL <thebaconator25@aim.com>
2023-05-28 22:29:31 -0400

    Removed main from Card class

--- Card.py
+++ Card.py
@@ -48,13 +48,3 @@ class MyCards:
 
     def get_card_by_index(self, index) -> Card:
         return self.cards[index]
-
-
-if __name__ == "__main__":
-    my_cards_collection = MyCards()
-    my_cards_collection.add_card(
-        "オーロラスカイ", "日野下花帆", "R", 1225, 1525, 925, 123, 100, "パフォーマー", "ハッピー"
-    )
-    # Get all the names of cards in our collection and print them out.
-    # print(my_cards_collection.get_card_names())
-    print(my_cards_collection.get_card_by_index(0).stats.mental)
