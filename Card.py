cdad8f5d135a60c82b6763aa8d64501909ed5198
LumineonRL <thebaconator25@aim.com>
2023-05-29 00:38:02 -0400

    Added json import/export functionality

--- Card.py
+++ Card.py
@@ -1,3 +1,5 @@
+import json
+
 class Card:
     def __init__(
         self,
@@ -11,13 +13,17 @@ class Card:
         bp: int,
         type: str,
         mood: str,
+        level: int = 1,
+        uncaps: int = 1,
+        appeal_level: int = 1,
+        skill_level: int = 1,
     ) -> None:
         self.name = name
         self.character = character
         self.rarity = rarity
         self.stats = self.Stats(smile, pure, cool, mental, bp)
         self.mood = self.Mood(type, mood)
-        self.training = self.Training()
+        self.training = self.Training(level, uncaps, appeal_level, skill_level)
 
     class Stats:
         def __init__(
@@ -33,43 +39,70 @@ class Card:
         def __init__(self, type: str, mood: str) -> None:
             self.type = type
             self.mood = mood
-            
+
     class Training:
         def __init__(
             self,
-            level: int = 1,
-            uncaps: int = 1,
-            appeal_level: int = 1,
-            skill_level: int = 1,
+            level,
+            uncaps,
+            appeal_level,
+            skill_level
         ):
             self.level = level
             self.uncaps = uncaps
             self.appeal_level = appeal_level
             self.skill_level = skill_level
-        
-
-            
 
 
 class MyCards:
-    def __init__(self):
+    def __init__(self) -> None:
         self.cards = []
 
-    def add_card(
-        self,
-        name: str,
-        character: str,
-        rarity: str,
-        s: int,
-        p: int,
-        c: int,
-        m: int,
-        b: int,
-        mood_type: str,
-        mood_mood: str,
-    ) -> None:
-        card = Card(name, character, rarity, s, p, c, m, b, mood_type, mood_mood)
+    def add_card(self, card: Card) -> None:
         self.cards.append(card)
+        
+    def remove_card(self, card: Card) -> None:
+        if card in self.cards:
+            self.cards.remove(card)
 
     def get_card_by_index(self, index) -> Card:
         return self.cards[index]
+    
+    def export_cards(self, filename: str) -> None:
+        data = []
+        for card in self.cards:
+            stats = card.stats.__dict__
+            mood = card.mood.__dict__
+            training = card.training.__dict__
+            
+            card_dict = {
+                "name": card.name,
+                "character": card.character,
+                "rarity": card.rarity,
+                "stats": stats,
+                "mood": mood,
+                "training": training
+            }
+            
+            data.append(card_dict)
+        
+        with open(filename, 'w') as f_out:
+            json.dump(data, f_out, default=lambda x: x.__dict__)
+    
+    def import_cards(self, filename:str)->None:
+        with open(filename, 'r') as f_in:
+            file = json.load(f_in)
+            
+        cards = []
+        
+        for data in file:
+            stats = data["stats"]
+            mood = data["mood"]
+            training = data["training"]
+            card = Card(data["name"], data["character"], data["rarity"], stats["smile"],
+                         stats["pure"], stats["cool"], stats["mental"], stats["bp"],
+                         mood["type"], mood["mood"], training["level"], training["uncaps"],
+                         training["appeal_level"], training["skill_level"])
+            cards.append(card)
+            
+        self.cards = cards
--- Main.py
+++ Main.py
@@ -1,16 +1,28 @@
-from Card import MyCards
+from Card import Card, MyCards
+import json
 
 if __name__ == "__main__":
     my_cards_collection = MyCards()
 
     try:
-        my_cards_collection.add_card(
-            "オーロラスカイ", "日野下花帆", "R", 1225, 1525, 925, 123, 100, "パフォーマー", "ハッピー"
-        )
+        my_card1 = Card("オーロラスカイ", "日野下花帆", "R", 1225, 1525, 925, 123, 100, "パフォーマー", "ハッピー")
     except Exception as e:
         print(e)
+        
+    my_cards_collection.add_card(my_card1)
+    
+    print(my_cards_collection.get_card_by_index(0).name)
+    my_cards_collection.export_cards("mycards.json")
+    
+    my_cards_imported = MyCards()
+    my_cards_import = my_cards_imported.import_cards("mycards.json")
+    
+    
+    print(my_cards_imported.get_card_by_index(0).name)
+    
+    
     # Get all the names of cards in our collection and print them out.
     # print(my_cards_collection.get_card_names())
-    print(my_cards_collection.get_card_by_index(0).training.level)
-    my_cards_collection.get_card_by_index(0).training.level = 100
-    print(my_cards_collection.get_card_by_index(0).training.level)
+    # print(my_cards_collection.get_card_by_index(0).training.level)
+    # my_cards_collection.get_card_by_index(0).training.level = 100
+    # print(my_cards_collection.get_card_by_index(0).training.level)
