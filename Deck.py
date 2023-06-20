8b6a567de8e99ecca6a669d9656d15872ec5ad7c
LumineonRL <thebaconator25@aim.com>
2023-06-20 01:20:20 -0400

    Changes to accomodate Card update.

--- Deck.py
+++ Deck.py
@@ -1,24 +1,65 @@
-from Card import Card
-from typing import List
 from collections import Counter
+from Card import Card
 
 
 class Deck:
     def __init__(self) -> None:
         INITIAL_CARDS = [
-            Card("オーロラスカイ", "日野下花帆", "R", 1225, 1525, 925, 123, 100, "パフォーマー", "ハッピー"),
-            Card("オーロラスカイ", "林野さやか", "R", 1225, 1025, 1425, 123, 100, "ムードメーカー", "メロウ"),
             Card(
-                "オーロラスカイ", "大沢瑠璃乃", "R", 1525, 1125, 725, 153, 100, "トリックスター", "ニュートラル"
+                "オーロラスカイ",
+                "日野下花帆",
+                "R",
+                {"smile": 1225, "pure": 1525, "cool": 925, "mental": 123, "bp": 100},
+                {"style_type": "パフォーマー", "mood": "ハッピー"},
+                {},
             ),
-            Card("オーロラスカイ", "乙宗梢", "R", 1525, 1225, 925, 123, 100, "チアリーダー", "ハッピー"),
-            Card("オーロラスカイ", "夕霧綴理", "R", 1225, 925, 1525, 123, 100, "ムードメーカー", "メロウ"),
-            Card("オーロラスカイ", "藤島慈", "R", 1125, 1525, 925, 133, 100, "チアリーダー", "ニュートラル"),
+            Card(
+                "オーロラスカイ",
+                "林野さやか",
+                "R",
+                {"smile": 1225, "pure": 1025, "cool": 1425, "mental": 123, "bp": 100},
+                {"style_type": "ムードメーカー", "mood": "メロウ"},
+                {},
+            ),
+            Card(
+                "オーロラスカイ",
+                "大沢瑠璃乃",
+                "R",
+                {"smile": 1525, "pure": 1125, "cool": 725, "mental": 153, "bp": 100},
+                {"style_type": "トリックスター", "mood": "ニュートラル"},
+                {},
+            ),
+            Card(
+                "オーロラスカイ",
+                "乙宗梢",
+                "R",
+                {"smile": 1525, "pure": 1225, "cool": 925, "mental": 123, "bp": 100},
+                {"style_type": "チアリーダー", "mood": "ハッピー"},
+                {},
+            ),
+            Card(
+                "オーロラスカイ",
+                "夕霧綴理",
+                "R",
+                {"smile": 1225, "pure": 925, "cool": 1525, "mental": 123, "bp": 100},
+                {"style_type": "ムードメーカー", "mood": "メロウ"},
+                {},
+            ),
+            Card(
+                "オーロラスカイ",
+                "藤島慈",
+                "R",
+                {"smile": 1125, "pure": 1525, "cool": 925, "mental": 133, "bp": 100},
+                {"style_type": "チアリーダー", "mood": "ニュートラル"},
+                {},
+            ),
         ]
 
         self.cards = []
         for card in INITIAL_CARDS:
             self.cards.append(card)
+
+        self._character_counts = Counter()
 
     def add_card(self, card: Card) -> None:
         self.cards.append(card)
@@ -47,13 +88,13 @@ class Deck:
 
         for char in CHARACTERS:
             if char not in self._character_counts.keys():
-                raise Exception(
+                raise ValueError(
                     f"{char} not found. You may have tried to delete all instances of a character from your deck."
                 )
 
     def _check_max_chars(self) -> None:
-        for char in self._character_counts.keys():
-            if self._character_counts[char] > 3:
-                raise Exception(
-                    f"More than 3 cards for {char} found. You may have tried to add a 4th card of a character to your deck.."
+        for char, count in self._character_counts.items():
+            if count > 3:
+                raise ValueError(
+                    f"More than 3 cards for {char} found. You may have tried to add a 4th card of a character to your deck."
                 )
--- Main.py
+++ Main.py
@@ -1,46 +1,109 @@
+from collections import Counter
 from Card import Card, MyCards
 from Deck import Deck
-import json
-from collections import Counter
 
 if __name__ == "__main__":
     my_cards_collection = MyCards()
 
     try:
         card01 = Card(
-            "オーロラスカイ", "日野下花帆", "R", 1225, 1525, 925, 123, 100, "パフォーマー", "ハッピー"
+            "オーロラスカイ",
+            "日野下花帆",
+            "R",
+            {"smile": 1225, "pure": 1525, "cool": 925, "mental": 123, "bp": 100},
+            {"style_type": "パフォーマー", "mood": "ハッピー"},
+            {},
         )
         card02 = Card(
-            "Rose Garden", "林野さやか", "UR", 1430, 2480, 2280, 113, 100, "ムードメーカー", "メロウ"
+            "Rose Garden",
+            "林野さやか",
+            "UR",
+            {"smile": 1430, "pure": 2480, "cool": 2280, "mental": 113, "bp": 100},
+            {"style_type": "ムードメーカー", "mood": "メロウ"},
+            {},
         )
+
         card03 = Card(
-            "オーロラスカイ", "林野さやか", "R", 1225, 1025, 1425, 123, 100, "ムードメーカー", "メロウ"
+            "オーロラスカイ",
+            "林野さやか",
+            "R",
+            {"smile": 1225, "pure": 1025, "cool": 1425, "mental": 123, "bp": 100},
+            {"style_type": "ムードメーカー", "mood": "メロウ"},
+            {},
         )
+
         card04 = Card(
-            "オーロラスカイ", "大沢瑠璃乃", "R", 1525, 1125, 725, 153, 100, "トリックスター", "ニュートラル"
+            "オーロラスカイ",
+            "大沢瑠璃乃",
+            "R",
+            {"smile": 1525, "pure": 1125, "cool": 725, "mental": 153, "bp": 100},
+            {"style_type": "トリックスター", "mood": "ニュートラル"},
+            {},
         )
+
         card05 = Card(
-            "チェリーピクニック", "乙宗梢", "UR", 2030, 2730, 930, 163, 100, "ムードメーカー", "ハッピー"
+            "チェリーピクニック",
+            "乙宗梢",
+            "UR",
+            {"smile": 2030, "pure": 2730, "cool": 930, "mental": 163, "bp": 100},
+            {"style_type": "ムードメーカー", "mood": "ハッピー"},
+            {},
         )
+
         card06 = Card(
-            "水彩世界", "乙宗梢", "SR", 1930, 2130, 1130, 173, 100, "ムードメーカー", "ハッピー"
+            "水彩世界",
+            "乙宗梢",
+            "SR",
+            {"smile": 1930, "pure": 2130, "cool": 1130, "mental": 173, "bp": 100},
+            {"style_type": "ムードメーカー", "mood": "ハッピー"},
+            {},
         )
+
         card07 = Card(
-            "オーロラスカイ", "乙宗梢", "R", 1525, 1225, 925, 123, 100, "チアリーダー", "ハッピー"
+            "オーロラスカイ",
+            "乙宗梢",
+            "R",
+            {"smile": 1525, "pure": 1225, "cool": 925, "mental": 123, "bp": 100},
+            {"style_type": "チアリーダー", "mood": "ハッピー"},
+            {},
         )
+
         card08 = Card(
-            "Rose Garden", "夕霧綴理", "SR", 1230, 2130, 2030, 193, 100, "ムードメーカー", "メロウ"
+            "Rose Garden",
+            "夕霧綴理",
+            "SR",
+            {"smile": 1230, "pure": 2130, "cool": 2030, "mental": 193, "bp": 100},
+            {"style_type": "ムードメーカー", "mood": "メロウ"},
+            {},
         )
+
         card09 = Card(
-            "AWOKE", "夕霧綴理", "SR", 2130, 1030, 1930, 183, 100, "ムードメーカー", "メロウ"
+            "AWOKE",
+            "夕霧綴理",
+            "SR",
+            {"smile": 2130, "pure": 1030, "cool": 1930, "mental": 183, "bp": 100},
+            {"style_type": "ムードメーカー", "mood": "メロウ"},
+            {},
         )
+
         card10 = Card(
-            "オーロラスカイ", "夕霧綴理", "R", 1225, 925, 1525, 123, 100, "ムードメーカー", "メロウ"
+            "オーロラスカイ",
+            "夕霧綴理",
+            "R",
+            {"smile": 1225, "pure": 925, "cool": 1525, "mental": 123, "bp": 100},
+            {"style_type": "ムードメーカー", "mood": "メロウ"},
+            {},
         )
+
         card11 = Card(
-            "オーロラスカイ", "藤島慈", "R", 1125, 1525, 925, 133, 100, "チアリーダー", "ニュートラル"
+            "オーロラスカイ",
+            "藤島慈",
+            "R",
+            {"smile": 1125, "pure": 1525, "cool": 925, "mental": 133, "bp": 100},
+            {"style_type": "チアリーダー", "mood": "ニュートラル"},
+            {},
         )
-    except Exception as e:
+    except ValueError as e:
         print(e)
 
     my_cards_collection.add_card(card01)
